10cdb59d68152510300d715f98f11b7e5f4345a8
rgchris <chris.rg@ross-gill.com>
2017-11-06 22:11:11 -0500

    CONTENT: Update RebSite index to hide missing sites

--- html/rebsites.r
+++ html/rebsites.r
@@ -1,24 +1,24 @@
-REBOL [Title: "New RebSite Index" Type: 'index]
+Rebol [Title: "New RebSite Index" Type: 'index]
 title "World Wide Reb Sites" 
 file " About " http://www.rebol.net/reb/about-sites.txt icon text info none 
 link " Add Site " https://github.com/revault/desktop.rebol.info/blob/master/html/rebsites.r icon rebol info "Add your site here"
 file " Site Notes" http://www.rebol.net/reb/site-note.txt icon text info "A note about empty sites." 
-folder "Rebol" http://www.rebol.com/index.r icon site info "REBOL Technologies" 
 folder "RebolBot" http://rebol.info/index.reb icon site info "RebolBot"
+folder "Rebol" http://www.rebol.com/index.r icon site info "REBOL Technologies" 
 folder "Library" http://www.rebol.org/library/reb/index.r icon site info "The shared script library"
 folder "Jeff's" http://www.cs.unm.edu/~whip/index.r icon site info none 
 folder "Code C." http://www.codeconscious.com/index.r icon site info "codeconscious.com" 
 folder "Anton" https://s3-ap-southeast-1.amazonaws.com/antonrolls/rebol/index.r icon site info {Lots of VID GUI styles, utilities, library functions, documentation.} 
 folder "Chris RG" http://www.ross-gill.com/index.r icon site info "Isle of Arran" 
-folder "H-O-H" http://www.h-o-h.org/index.r icon site info "Some Rebol Scripts by Ingo Hohmann"
-folder "REBolek" http://www.sweb.cz/rebolek/index.r icon site info "REBolek scripts"
+; folder "H-O-H" http://www.h-o-h.org/index.r icon site info "Some Rebol Scripts by Ingo Hohmann"
+; folder "REBolek" http://www.sweb.cz/rebolek/index.r icon site info "REBolek scripts"
 folder "Fantam" http://perso.worldonline.fr/mutant/Code/rebol/index.r icon site info "A few scripts..." 
-folder "Rebol.it" http://www.rebol.it/index.r icon site info "The italian REB site - Il sito REB italiano" 
+; folder "Rebol.it" http://www.rebol.it/index.r icon site info "The italian REB site - Il sito REB italiano" 
 folder "Etienne" http://etienne.alaurent.free.fr/index.r icon site info {Home of "VIRTUAL Office" project} 
 folder "Romano" http://web.tiscali.it/anarkick/index.r icon site info "AnaMonitor House" 
 folder "pat665" http://www.pat665.free.fr/index.r icon site info "pat665 rebsite: faq, rugby getting started doc." 
 folder "polux free" http://polux2.free.fr/rebol/index.r icon site info "A few words about your site go here." 
-folder "Compkarori2" http://www.compkarori.com/cgi-local/index.r icon site info "Compkarori's CGI scripted rebsite" 
+; folder "Compkarori2" http://www.compkarori.com/cgi-local/index.r icon site info "Compkarori's CGI scripted rebsite" 
 folder "Alternative" http://gregory.pecheret.free.fr/index.r icon site info "Welcome!" 
 folder "DocKimbel" http://rebol.softinnov.org/index.r icon site info "Home of MySQL scheme, Win95 skin, Vector Balls,..." 
 folder "RebolZone" http://rebolzone.free.fr/index.r icon site info "Site dédié à Rebol" 
@@ -26,18 +26,18 @@ folder "ReViewer" http://www.digicamsoft.com/index.r icon site info "Build, Sort
 folder "reziztanzia" http://reziztanzia.free.fr/index.r icon site info "reziztanzia RebolCoincoin site" 
 folder "AGReb" http://alain.goye.free.fr/rebol/index.r icon site info "A few ideas about email, databases, interfaces..." 
 folder "Fabrice" http://vado.fabrice.free.fr/rebsite/index.r icon site info "Fab RebSite" 
-folder "Rebolution" http://home.zonnet.nl/rebolution/rebol/index.r icon site info "Promotion of REBOL" 
+; folder "Rebolution" http://home.zonnet.nl/rebolution/rebol/index.r icon site info "Promotion of REBOL" 
 folder "REBALL" http://lauretprod.web.ool.fr/manuweb/index.r icon site info {Bienvenue sur REBALL de http://www.lauretprod.fr.st by Satcorp} 
 folder "Coeurps" http://perso.wanadoo.fr/internethique/rebol/index.r icon site info "Little reb will become bigger" 
 folder "RM2" http://richard.menu.free.fr/rebol/index.r icon site info "Rebol/RAD dvlp Project" 
 folder "plegoff" http://pl.legoff.free.fr/docs/informatique/rebol/index.r icon site info "home of plegoff" 
 folder "TGD" http://www.tgd-consulting.de/index.r icon site info {TGD-Consulting' s REBsite: Home of Serve-It! and fancy REBgames & REBtools. Take a look @ ...} 
-folder "Kilter" http://kilter.memebot.com/rebsite/index.r icon site info "Kilter's Kastle" 
+; folder "Kilter" http://kilter.memebot.com/rebsite/index.r icon site info "Kilter's Kastle" 
 folder "Sguish" http://sguish.50webs.org/index.r icon site info "This is an italian site dedicated to Rebol!" 
 folder "Luis" http://luisrodriguezjurado.googlepages.com/index.r icon site info "Luis" 
 folder "Andwo" http://www.valknut.org/index.r icon site info {development of a free of charge healing service with the help of rebol} 
-folder "UAForth" http://forth.org.ua/index.r icon site info "Ukrainian Forth Interest Group" 
+; folder "UAForth" http://forth.org.ua/index.r icon site info "Ukrainian Forth Interest Group" 
 folder "LDCI" http://perso.numericable.fr/frajouen/index.r icon site info "A few words about your site go here." 
 folder "MaxV" http://www.maxvessi.net/rebsite/index.r icon site info "My works and ideas" 
 folder "Jplprog" http://www.jplprog.com/rebol/index.r icon site info "My web reb site with applications." 
-folder "SalWare" http://salware.altervista.org/Rebol/index.r icon site info "SalWare Rebol Site"
+; folder "SalWare" http://salware.altervista.org/Rebol/index.r icon site info "SalWare Rebol Site"
